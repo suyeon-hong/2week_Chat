ed1067974cc1532ebc65be8802fd5a3534095c82
Jeongs <kimm.jeongs@gmail.com>
2022-02-10 19:26:19 +0900

    :sparkles:  Feat: Route 설정, Chats 컴포넌트 default Route 설정
    Closes #8

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import Test from '@components/Test/Test';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Chats, Login } from '@pages';
 import '@styles/index.scss';
 
 function App() {
-  return <Test />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Chats />}></Route>
+        <Route path="login" element={<Login />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
