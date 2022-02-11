228507a27e29f9469e63fe7c1dc1fede0f2aa50d
Jeongs <kimm.jeongs@gmail.com>
2022-02-11 16:57:44 +0900

    :sparkles:  UI, Feat: MessagesBox 레이아웃 수정, getFormattedDate 함수 적용

--- src/pages/Chats/Chats.tsx
+++ src/pages/Chats/Chats.tsx
@@ -10,8 +10,9 @@ const messages = [
     userId: '1',
     userName: '12',
     profileImage: '123',
-    content: '123',
-    date: '123',
+    content: `배고프다 
+      커피커피 호로로 졸려 라랄라라라라랄 말이 엄청 길어지면 어떻게 될까? 배고프다 귀찮아 졸려 라랄라라라라랄 말이 엄청 길어지면 어떻게 될까?`,
+    date: new Date(),
   },
 ];
 
@@ -29,7 +30,6 @@ const Chats = () => {
         {React.Children.toArray(
           messages.map((message) => (
             <MessagesBox
-              key={message.chatId}
               message={message}
               handleDeleteModal={handleDeleteModal}
               handleReply={handleReply}
