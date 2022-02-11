c8a390ce4c5324f5460ef5e84590396774e5f885
ludacirs <ludacirs@naver.com>
2022-02-11 13:57:40 +0900

    🐛 Fix: 타입 절대경로 수정

--- src/types/EventTypes.ts
+++
@@ -1,9 +0,0 @@
-export enum KeyCodeType {
-  NumpadEnter = 'NumpadEnter',
-  Enter = 'Enter',
-  ESC = 'Esc',
-}
-
-export enum InputEventType {
-  insertLineBreak = 'insertLineBreak',
-}
--- src/types/index.ts
+++ src/types/index.ts
@@ -3,6 +3,16 @@
 export interface UserData {
   userId: string;
   userName: string;
+  chatId: number;
   content: string;
   date: Date;
+}
+export enum KeyCodeType {
+  NumpadEnter = 'NumpadEnter',
+  Enter = 'Enter',
+  ESC = 'Esc',
+}
+
+export enum InputEventType {
+  insertLineBreak = 'insertLineBreak',
 }
